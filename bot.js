44f94a199cac83cbbb086c12e499b94dd38adfb9
KSA3 <42389626+KSA3@users.noreply.github.com>
2018-09-12 16:00:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -439,22 +439,22 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if(message.content === 'IP'){
-        message.channel.send('قريبا')
+        message.channel.send('185.249.198.195:30120')
     }
 });
 client.on('message', message => {
     if(message.content === 'Ip'){
-        message.channel.send('قريبا')
+        message.channel.send('185.249.198.195:30120')
     }
 });
 client.on('message', message => {
     if(message.content === 'ip'){
-        message.channel.send('قريبا')
+        message.channel.send('185.249.198.195:30120')
     }
 });
 client.on('message', message => {
     if(message.content === 'iP'){
-        message.channel.send('قريبا')
+        message.channel.send('185.249.198.195:30120')
     }
 });
 client.on('message', message => {
