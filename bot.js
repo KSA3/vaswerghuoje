397573fa15e650cf77892be1ee1c5cd9c7c6f203
KSA3 <42389626+KSA3@users.noreply.github.com>
2018-09-17 17:53:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1555,20 +1555,4 @@ if(message.content.startsWith(prefix + 'unhackban')) {
   }).catch(() => message.channel.send("Theres no user with the this ID :face_palm:"))
 }
   })
-const D = require("discord.js");
-const client = new D.Client();
-const moment = require("moment")
-client.on("guildMemberAdd", m => {
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-        m.ban();
-    };
-});
-function parseDate(str) {
-    var mdy = str.split('/');
-    return new Date(mdy[2], mdy[0]-1, mdy[1]);
-};
-
-function datediff(first, second) {
-    return Math.round((second-first)/(1000*60*60*24));
-};
 client.login(process.env.BOT_TOKEN);
