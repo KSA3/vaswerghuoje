4fcae7553cba45544655ef127c82d086f5c29f1d
KSA3 <42389626+KSA3@users.noreply.github.com>
2018-09-19 17:29:55 +0300

    Update xo.js

--- Games/xo.js
+++ Games/xo.js
@@ -1,119 +1,166 @@
-const request = require('snekfetch');
-const crypto = require('crypto');
-const { IMGUR_KEY } = process.env;
-const yes = ['yes', 'y', 'ye', 'yeah', 'yup', 'yea'];
-const no = ['no', 'n', 'nah', 'nope'];
+module.exports = (client, message, new_message, player1_id, player2_id, turn_id, symbol, symbols, grid_message) => {
 
-class Extend {
-	static wait(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms));
-	}
+  var score = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+  var will_end_game = false;
 
-	static shuffle(array) {
-		const arr = array.slice(0);
-		for (let i = arr.length - 1; i >= 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			const temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}
-		return arr;
-	}
+  client.on('messageReactionAdd', (reaction, user) => {
+    if (reaction.message.id == new_message.id && turn_id == user.id && !will_end_game) {
 
-	static list(arr, conj = 'and') {
-		const len = arr.length;
-		return `${arr.slice(0, -1).join(', ')}${len > 1 ? `${len > 2 ? ',' : ''} ${conj} ` : ''}${arr.slice(-1)}`;
-	}
+      let emoji;
+      // Convert emoji identifier to :emoji: format
+      switch(reaction.emoji.identifier) {
+        case '1%E2%83%A3':
+          emoji = ':one:';
+          if (score[0][0] == null) score[0][0] = symbol;
+          break;
+        case '2%E2%83%A3':
+          emoji = ':two:';
+          if (score[0][1] == null) score[0][1] = symbol;
+          break;
+        case '3%E2%83%A3':
+          emoji = ':three:';
+          if (score[0][2] == null) score[0][2] = symbol;
+          break;
+        case '4%E2%83%A3':
+          emoji = ':four:';
+          if (score[1][0] == null) score[1][0] = symbol;
+          break;
+        case '5%E2%83%A3':
+          emoji = ':five:';
+          if (score[1][1] == null) score[1][1] = symbol;
+          break;
+        case '6%E2%83%A3':
+          emoji = ':six:';
+          if (score[1][2] == null) score[1][2] = symbol;
+          break;
+        case '7%E2%83%A3':
+          emoji = ':seven:';
+          if (score[2][0] == null) score[2][0] = symbol;
+          break;
+        case '8%E2%83%A3':
+          emoji = ':eight:';
+          if (score[2][1] == null) score[2][1] = symbol;
+          break;
+        case '9%E2%83%A3':
+          emoji = ':nine:';
+          if (score[2][2] == null) score[2][2] = symbol;
+          break;
+        default:
+          break;
+      }
 
-	static shorten(text, maxLen = 2000) {
-		return text.length > maxLen ? `${text.substr(0, maxLen - 3)}...` : text;
-	}
+      // Replace number tile with O or X (checks if it exists first)
+      if (grid_message.content.indexOf(emoji) == -1) {
+        return;
+      }
+      grid_message.edit(grid_message.content.replace(emoji, symbol))
+      .then((new_mes) => {
+        grid_message = new_mes;
+        console.log("Successful # tile to symbol switch");
+      })
+      .catch(console.error);
 
-	static duration(ms) {
-		const sec = Math.floor((ms / 1000) % 60).toString();
-		const min = Math.floor((ms / (1000 * 60)) % 60).toString();
-		const hrs = Math.floor(ms / (1000 * 60 * 60)).toString();
-		return `${hrs.padStart(2, '0')}:${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
-	}
+      // Check if the game has concluded
+      if (didPlayerWin(symbols[0], player1_id) || didPlayerWin(symbols[1], player2_id) || didItTie()) {
+        will_end_game = true;
+        return;
+      }
 
-	static randomRange(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+      // Replace player with the next and symbol with the next
+      let temp_message = new_message.content.replace(`<@${turn_id}>`, `<@${toggle_player(turn_id, player1_id, player2_id)}>`);
+      temp_message = temp_message.replace(symbol, toggle_symbol(symbol));
+      new_message.edit(temp_message)
+      .then(console.log("Successful turn switch"))
+      .catch(console.error);
 
-	static trimArray(arr, maxLen = 10) {
-		if (arr.length > maxLen) {
-			const len = arr.length - maxLen;
-			arr = arr.slice(0, maxLen);
-			arr.push(`${len} more...`);
-		}
-		return arr;
-	}
+      // Toggle symbols between O and X and players 1 and 2
+      symbol = toggle_symbol(symbol);
+      turn_id = toggle_player(turn_id, player1_id, player2_id);
+    }
+  })
 
-	static base64(text, mode = 'encode') {
-		if (mode === 'encode') return Buffer.from(text).toString('base64');
-		if (mode === 'decode') return Buffer.from(text, 'base64').toString('utf8') || null;
-		throw new TypeError(`${mode} is not a supported base64 mode.`);
-	}
 
-	static hash(text, algorithm) {
-		return crypto.createHash(algorithm).update(text).digest('hex');
-	}
+  // Function for toggling players
+  function toggle_player(turn_id, player1_id, player2_id) {
+    let player_switched;
+    if (turn_id == player1_id) {
+      player_switched = player2_id;
+    }
+    else {
+      player_switched = player1_id;
+    }
+    return player_switched;
+  }
 
-	static async randomFromImgurAlbum(album) {
-		const { body } = await request
-			.get(`https://api.imgur.com/3/album/${album}`)
-			.set({ Authorization: `Client-ID ${IMGUR_KEY}` });
-		if (!body.data.images.length) return null;
-		return body.data.images[Math.floor(Math.random() * body.data.images.length)].link;
-	}
+  // Function for toggling symbols
+  function toggle_symbol(symbol) {
+    return symbols[Math.abs(symbols.findIndex((sym) => {
+      return sym == symbol;
+    }) - 1)];
+  }
 
-	static today(timeZone) {
-		const now = new Date();
-		if (timeZone) now.setUTCHours(now.getUTCHours() + timeZone);
-		now.setHours(0);
-		now.setMinutes(0);
-		now.setSeconds(0);
-		now.setMilliseconds(0);
-		return now;
-	}
+  // Function for checking if a player won
+  function didPlayerWin(sym, player) {
+    for (let i = 0; i < score.length; i++) {
+      // Horizontal checks
+      if (score[i][0] == sym &&
+          score[i][1] == sym &&
+          score[i][2] == sym) {
+            new_message.edit(`Congratulations! <@${player}> won!`)
+            .then(console.log('Successful win'))
+            .catch(console.error);
+            return true;
+      }
+      // Vertical checks
+      else if (score[0][i] == sym &&
+               score[1][i] == sym &&
+               score[2][i] == sym) {
+               new_message.edit(`Congratulations! <@${player}> won!`)
+               .then(console.log('Successful win'))
+               .catch(console.error);
+               return true;
+      }
+    }
+    // Diagonal checks
+    if (score[0][0] == sym &&
+        score[1][1] == sym &&
+        score[2][2] == sym) {
+          new_message.edit(`Congratulations! <@${player}> won!`)
+          .then(console.log('Successful win'))
+          .catch(console.error);
+          return true;
+    }
+    else if (score[0][2] == sym &&
+             score[1][1] == sym &&
+             score[2][0] == sym) {
+               new_message.edit(`Congratulations! <@${player}> won!`)
+               .then(console.log('Successful win'))
+               .catch(console.error);
+               return true;
+    }
 
-	static tomorrow(timeZone) {
-		const today = Util.today(timeZone);
-		today.setDate(today.getDate() + 1);
-		return today;
-	}
+    return false;
+  }
 
-	static async awaitPlayers(msg, max, min, { text = 'join game', time = 30000 } = {}) {
-		const joined = [];
-		joined.push(msg.author.id);
-		const filter = res => {
-			if (msg.author.bot) return false;
-			if (joined.includes(res.author.id)) return false;
-			if (res.content.toLowerCase() !== text.toLowerCase()) return false;
-			joined.push(res.author.id);
-			return true;
-		};
-		const verify = await msg.channel.awaitMessages(filter, { max, time });
-		verify.set(msg.id, msg);
-		if (verify.size < min) return false;
-		return verify.map(message => message.author);
-	}
-
-	static async verify(channel, user, time = 30000) {
-		const filter = res => {
-			const value = res.content.toLowerCase();
-			return res.author.id === user.id && (yes.includes(value) || no.includes(value));
-		};
-		const verify = await channel.awaitMessages(filter, {
-			max: 1,
-			time
-		});
-		if (!verify.size) return 0;
-		const choice = verify.first().content.toLowerCase();
-		if (yes.includes(choice)) return true;
-		if (no.includes(choice)) return false;
-		return false;
-	}
+  // Function for checking if it's a tie
+  function didItTie() {
+    let null_counter = 0;
+    for (let i = 0; i < score.length; i++) {
+      for (let j = 0; j < score.length; j++) {
+        if (score[i][j] == null) {
+          null_counter++;
+        }
+      }
+    }
+    if (null_counter == 0) {
+      new_message.edit('Boo! It\'s a tie!')
+      .then(console.log('Successful tie'))
+      .catch(console.error);
+      return true;
+    }
+  }
 }
-
-module.exports = Extend;
